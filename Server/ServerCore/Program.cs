9661be9a5fa806c5fdadbd2283113a25dd887cfb
pkh <tapasse12@gmail.com>
2026-05-21 00:44:27 +0900

    feat: ThreadPool 사용

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -1,8 +1,8 @@
 ﻿namespace ServerCore
 {
-    internal class Program
+    class Program
     {
-        static void MainThread()
+        static void MainThread(object state)
         {
             for(int i=0; i<5; i++)
                 Console.WriteLine("Thread Running!");
@@ -10,7 +10,20 @@
 
         static void Main(string[] args)
         {
-            Thread t = new Thread(MainThread);
+            ThreadPool.SetMinThreads(1, 1); // 최소 스레드 1
+            ThreadPool.SetMaxThreads(5, 5); // 최대 스레드 5
+
+            for (int i = 0; i < 5; i++)
+                ThreadPool.QueueUserWorkItem((obj) => { Console.WriteLine("Queue Running"); });
+
+            ThreadPool.QueueUserWorkItem(MainThread); // 완료된 스레드가 
+
+            while(true) // QueueUserWorkItem은 기본적으로 Background(IsBackground = true)이므로, 종료 방지
+            {
+
+            }
+
+            /*Thread t = new Thread(MainThread);
             t.Name = "Test Thread";
             t.IsBackground = true;
             t.Start();
@@ -18,7 +31,7 @@
             Console.WriteLine("Thread Start!");
 
             t.Join();
-            Console.WriteLine("End Complete!");
+            Console.WriteLine("End Complete!");*/
         }
     }
 }
