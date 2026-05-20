a4343bc4bdbeca63b1d7eccc773c44f3b344bf5b
pkh <tapasse12@gmail.com>
2026-05-21 00:14:52 +0900

    feat: 쓰레드 연습

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -2,9 +2,23 @@
 {
     internal class Program
     {
+        static void MainThread()
+        {
+            for(int i=0; i<5; i++)
+                Console.WriteLine("Thread Running!");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Thread t = new Thread(MainThread);
+            t.Name = "Test Thread";
+            t.IsBackground = true;
+            t.Start();
+
+            Console.WriteLine("Thread Start!");
+
+            t.Join();
+            Console.WriteLine("End Complete!");
         }
     }
 }
