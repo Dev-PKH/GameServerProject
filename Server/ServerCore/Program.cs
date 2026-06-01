042898166dd1cf08de64bf94943af24ec6b58d36
pkh <tapasse12@gmail.com>
2026-06-02 00:22:50 +0900

    feat: SpinLock, Lock, In/Decrement 비용 테스트

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ServerCore
+﻿using System;
+
+namespace ServerCore
 {
     class SpinLock
     {
@@ -8,9 +10,6 @@
         {
             while (true)
             {
-                // Interlocked.Exchange(ref int, value): int의 현재 값을 반환하며, int값을 value로 바꿈
-                // ex) locked 0일때, int original = Interlocked.Exchange(ref locked, 1) 실행 결과
-                // original: 0(Exchange이전 locked값), locekd = 1(Exchange Value(1)값을 적용)
                 int original = Interlocked.Exchange(ref locked, 1);
                 if (original == 0) break;
             }
@@ -24,12 +23,14 @@
 
     class Program
     {
-        static int num = 0;
+        static int num = 0; // 
+        static object lObj = new object(); // lock Object
         static SpinLock spinLock = new SpinLock();
 
-        static void Thread1()
+        // Spin Lock Thread
+        static void SThread1()
         {
-            for(int i=0; i<100000; i++)
+            for (int i = 0; i < 100000; i++)
             {
                 spinLock.Acquire();
                 num++;
@@ -37,7 +38,7 @@
             }
         }
 
-        static void Thread2()
+        static void SThread2()
         {
             for (int i = 0; i < 100000; i++)
             {
@@ -47,17 +48,85 @@
             }
         }
 
+        // Interlocked.Increment, Decrement Thread
+        static void IThread1()
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                Interlocked.Increment(ref num);
+            }
+        }
+
+        static void IThread2()
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                Interlocked.Decrement(ref num);
+            }
+        }
+
+        // Locked Thread
+
+        static void LThread1()
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                lock (lObj)
+                {
+                    num++;
+                }
+            }
+        }
+
+        static void LThread2()
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                lock (lObj)
+                {
+                    num--;
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-            Task t1 = new Task(Thread1);
-            Task t2 = new Task(Thread2);
+            long now = DateTime.Now.Ticks;
+            Task st1 = new Task(SThread1);
+            Task st2 = new Task(SThread2);
 
-            t1.Start();
-            t2.Start();
+            st1.Start();
+            st2.Start();
 
-            Task.WaitAll(t1, t2);
+            Task.WaitAll(st1, st2);
+            long cur = DateTime.Now.Ticks;
+            Console.WriteLine($"SpinLock Tick: {cur - now}");
 
-            Console.WriteLine(num);
+            num = 0; // 초기화 방어 코드
+
+            now = DateTime.Now.Ticks;
+            Task it1 = new Task(IThread1);
+            Task it2 = new Task(IThread2);
+
+            it1.Start();
+            it2.Start();
+
+            Task.WaitAll(it1, it2);
+            cur = DateTime.Now.Ticks;
+            Console.WriteLine($"Interlocked(In/Decrement) Tick: {cur - now}");
+
+            num = 0; // 초기화 방어 코드
+
+            now = DateTime.Now.Ticks;
+            Task lt1 = new Task(LThread1);
+            Task lt2 = new Task(LThread2);
+
+            lt1.Start();
+            lt2.Start();
+
+            Task.WaitAll(lt1, lt2);
+            cur = DateTime.Now.Ticks;
+            Console.WriteLine($"Lock Tick: {cur - now}");
         }
     }
 }
