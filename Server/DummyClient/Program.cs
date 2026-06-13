d27567d6f643c07529c7aff9ab67abebb7968b4f
pkh <tapasse12@gmail.com>
2026-06-13 23:05:28 +0900

    feat: 소켓 프로그래밍 연습

--- Server/DummyClient/Program.cs
+++ Server/DummyClient/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace DummyClient
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace DummyClient
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            // 입장권 생성 (Client 전용 Socket)
+            Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                // 차단봉(서버 listen Socket) 접근
+                socket.Connect(endPoint);
+                Console.WriteLine($"Connected TO {socket.RemoteEndPoint.ToString()}");
+
+                // 관리자(Server)로 데이터 송신
+                byte[] sendBuff = Encoding.UTF8.GetBytes("Parking complete!~");
+                int sendBytes = socket.Send(sendBuff);
+
+                // 관리자의 데이터 수신
+                byte[] recvBuff = new byte[1024];
+                int recvBytes = socket.Receive(recvBuff);
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                Console.WriteLine($"[From Server] {recvData}");
+
+                // 퇴장
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
     }
 }
--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -1,30 +1,58 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace ServerCore
 {
     class Program
     {
-        static ThreadLocal<string> name = new(
-            () => { return $"Name: {Thread.CurrentThread.ManagedThreadId}"; });
-        // tls에서 새롭게 할당받을 때 마다, 현재 스레드의 id를 반환
-
-        static void GetName()
-        {
-            bool repeat = name.IsValueCreated; // 현재 Thread가 별도의 공간을 마련했는지
-            if (repeat)
-                Console.WriteLine(name.Value + "(repeat)"); // 해당 변수를 사용
-            else
-                Console.WriteLine(name.Value); // 자신만의 공간을 만들고 사용
-        }
-
-
         static void Main(string[] args)
         {
-            ThreadPool.SetMinThreads(1, 1);
-            ThreadPool.SetMaxThreads(3, 3);
-            Parallel.Invoke(GetName, GetName, GetName, GetName, GetName); // task 5개 실행과 동일
+            // DNS로 ip 추출 ex) www.google.com -> 142.251.152.119
+            string host = Dns.GetHostName(); // 내 local 컴퓨터의 host 이름
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
-            name.Dispose();
+            // 1. 차단봉(listen Socket) 생성
+            // [공부] Socket에서 Stream 타입이 뭔가?, 다른 타입이 뭐가 있지
+            Socket listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                // 2. 차단봉 설치(Bind)
+                listenSocket.Bind(endPoint);
+
+                // 3. 차단봉 설정 완료, backlog: 최대 대기수
+                listenSocket.Listen(10);
+
+                while (true)
+                {
+                    Console.WriteLine("=======Listening=======");
+
+                    // 차 입장 (Client)
+                    Socket clientSocket = listenSocket.Accept(); // <- 클라이언트가 입장 안하면 아래로 안내려감 (실제론 이렇게 구현하지 않음)
+
+                    // 차의 데이터 수신
+                    byte[] recvBuff = new byte[1024];
+                    int recvBytes = clientSocket.Receive(recvBuff);
+                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                    Console.WriteLine($"[Client Receive Data] {recvData}");
+
+                    // 차로 데이터 송신
+                    byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to Parking Server~!");
+                    clientSocket.Send(sendBuff);
+
+                    // 차 퇴장(강제)
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
     }
 }
