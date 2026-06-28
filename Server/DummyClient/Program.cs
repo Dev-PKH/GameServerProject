55fddfdee8abde53b656e7d85b7d371776cc5230
pkh <tapasse12@gmail.com>
2026-06-28 21:29:21 +0900

    feat: Connector 구현

--- Server/DummyClient/Program.cs
+++ Server/DummyClient/Program.cs
@@ -1,9 +1,39 @@
-﻿using System.Net;
+﻿using ServerCore;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
 namespace DummyClient
 {
+    class GameSession : Session
+    {
+        public override void OnConnected(EndPoint endPoint)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                // 관리자(Server)로 데이터 송신
+                byte[] sendBuff = Encoding.UTF8.GetBytes($"Parking complete!~ {i}");
+                Send(sendBuff);
+            }
+        }
+
+        public override void OnDisconnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"연결 해제: {endPoint}");
+        }
+
+        public override void OnRecive(ArraySegment<byte> buffer)
+        {
+            string reciveData = Encoding.UTF8.GetString(buffer.Array, buffer.Offset, buffer.Count); // 버퍼 크기, 시작 위치, 바이트 개수
+            Console.WriteLine($"[서버가 받을 정보] {reciveData}");
+        }
+
+        public override void OnSend(int numOfBytes)
+        {
+            Console.WriteLine($"사용된 바이트 수: {numOfBytes}");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -13,33 +43,14 @@ namespace DummyClient
             IPAddress ipAddr = ipHost.AddressList[0];
             IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
+            Connector connector = new();
+            connector.Connect(endPoint, () => new GameSession());
+
             while(true)
             {
-                // 입장권 생성 (Client 전용 Socket)
-                Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-
                 try
                 {
-                    // 차단봉(서버 listen Socket) 접근
-                    socket.Connect(endPoint);
-                    Console.WriteLine($"Connected TO {socket.RemoteEndPoint.ToString()}");
-
-                    for(int i=0; i<5; i++)
-                    {
-                        // 관리자(Server)로 데이터 송신
-                        byte[] sendBuff = Encoding.UTF8.GetBytes($"Parking complete!~ {i}");
-                        int sendBytes = socket.Send(sendBuff);
-                    }
-
-                    // 관리자의 데이터 수신
-                    byte[] recvBuff = new byte[1024];
-                    int recvBytes = socket.Receive(recvBuff);
-                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
-                    Console.WriteLine($"[From Server] {recvData}");
-
-                    // 퇴장
-                    socket.Shutdown(SocketShutdown.Both);
-                    socket.Close();
+                   
                 }
                 catch (Exception ex)
                 {
---
+++ Server/ServerCore/Connector.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace ServerCore
+{
+    public class Connector
+    {
+        Func<Session> sessionFactory;
+
+        public void Connect(IPEndPoint endPoint, Func<Session> sessionFactory)
+        {
+            Socket socket = new(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            this.sessionFactory = sessionFactory;
+
+            SocketAsyncEventArgs args = new();
+            args.Completed += OnConnectCompleted;
+            args.RemoteEndPoint = endPoint;
+            args.UserToken = socket;
+
+            RegisterConnect(args);
+        }
+
+        void RegisterConnect(SocketAsyncEventArgs args)
+        {
+            Socket socket = args.UserToken as Socket;
+            if (socket == null) return;
+
+
+            bool pending = socket.ConnectAsync(args);
+            if(pending == false)
+                OnConnectCompleted(null, args);
+
+        }
+
+        void OnConnectCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            if(args.SocketError == SocketError.Success)
+            {
+                Session session = sessionFactory.Invoke();
+                session.Start(args.ConnectSocket);
+                session.OnConnected(args.RemoteEndPoint);
+            }
+            else
+            {
+                Console.WriteLine($"Connected Fail: {args.SocketError}");
+            }
+        }
+    }
+}
